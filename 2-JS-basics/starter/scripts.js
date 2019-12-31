b40fff82fedae4a019aafa40d55effbd26fcd526
pogikeith <pogikeith@yahoo.com>
2019-12-31 13:18:39 -0700

    two

--- 2-JS-basics/starter/scripts.js
+++ 2-JS-basics/starter/scripts.js
@@ -1,1 +1,14 @@
-console.log('hello world!!!'); 
+var firstName = 'john'; 
+console.log(firstName); 
+
+var lastName = 'smith'; 
+var age = 25; 
+
+var fullAge = true; 
+console.log(fullAge); 
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job); 
