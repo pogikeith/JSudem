d120dbfb17178b8e5bbcbcf0bb1b14f04ec24822
pogikeith <pogikeith@yahoo.com>
2020-01-08 21:35:55 -0700

    boolean

---
+++ 2-JS-basics/starter/ifelse.js
@@ -0,0 +1,17 @@
+var firstName = 'john'; 
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married'); 
+}   
+    else {
+        console.log(firstName + ' will hopefully'); 
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married'); 
+}   
+    else {
+        console.log(firstName + ' will hopefully'); 
+}
--- 2-JS-basics/starter/operators.js
+++ 2-JS-basics/starter/operators.js
@@ -1,33 +1,47 @@
 // */math operartors*/
 
-var year, yearJohn, yearMark; 
+// var year, yearJohn, yearMark; 
 
-now = 2020; 
-var yearJohn = now - 28; 
-var yearMark = now - 33; 
+// now = 2020; 
+// var yearJohn = now - 28; 
+// var yearMark = now - 33; 
 
-console.log(yearJohn); 
+// console.log(yearJohn); 
 
-console.log(now + 2); 
-console.log(now * 2); 
-console.log(now / 10); 
+// console.log(now + 2); 
+// console.log(now * 2); 
+// console.log(now / 10); 
 
 // */logical operartors*/
 
-var year, yearJohn, yearMark; 
+// var year, yearJohn, yearMark; 
 
-now = 2018; 
-ageJohn = 28; 
-ageMark = 33; 
+// now = 2018; 
+// ageJohn = 28; 
+// ageMark = 33; 
 
-yearJohn = now - ageJohn;
-yearMark = now - ageMark;
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
 
-console.log(yearJohn); 
+// console.log(yearJohn); 
 
-console.log(now + 2); 
-console.log(now * 2); 
-console.log(now / 10); 
+// console.log(now + 2); 
+// console.log(now * 2); 
+// console.log(now / 10); 
 
-var johnOlder = ageJohn > ageMark; 
-console.log(johnOlder); 
+// var johnOlder = ageJohn > ageMark; 
+// console.log(johnOlder); 
+
+// var now = 2018;
+// var yearJohn = 1989;
+// var fullAge = 18;
+
+// Multiple operators
+// var isFullAge = now - yearJohn >= fullAge; // true
+// console.log(isFullAge);
+
+// // Grouping
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+// var average = (ageJohn + ageMark) / 2;
+// console.log(average);
