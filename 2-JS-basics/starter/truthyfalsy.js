e7bad467e3500ef6cff2da5f0cc46b1dfa2752ed
pogikeith <pogikeith@yahoo.com>
2020-01-09 09:27:42 -0700

    truthy

---
+++ 2-JS-basics/starter/truthyfalsy.js
@@ -0,0 +1,14 @@
+//Truthy and Falsy values and equality operators
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+height = 23;
+
+if (height || height === 0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable has NOT been defined');
+}
